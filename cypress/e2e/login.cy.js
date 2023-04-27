31b41ceee4fa27b5158fe519cbc83eee5d6059c7
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2023-04-26 21:18:27 -0300

    First commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+/// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
+
+context('Funcionalidade Login', () => {
+
+    beforeEach(() => {
+        cy.visit('minha-conta')
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('Login com sucesso usando Comando customizado', () => {
+        cy.login('aluno_ebac@teste.com', 'teste@teste.com')
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+
+    it('Login usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+        
+            cy.get('.page-title').should('contain', 'Minha conta')
+            cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá')
+        })
+    });
+
+    it('Login usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.page-title').should('contain', 'Minha conta')
+    });
+
+
+    it('Deve fazer login com sucesso - sem otimização', () => {
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com', { log: false })
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+    })
+})
