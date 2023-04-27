6c2672919614318fd3dc67a3066384cfc7b6d593
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2023-04-26 21:40:42 -0300

    Teste automatizado realização de pedido

--- cypress/e2e/e2e.cy.js
+++ cypress/e2e/e2e.cy.js
@@ -10,12 +10,33 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         E validando minha compra ao final */
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('produtos')
+    });
+
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('[class="product-block grid"]').first().click()
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        cy.get('[class="product-block grid"]').first().click()
+        cy.get('.button-variable-item-XS').click()
+        cy.get('.button-variable-item-Red').click()
+        cy.get('.input-text').clear().type('4')
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.woocommerce-message > .button').click()
+        cy.get('.checkout-button').click()
+        cy.get('.showlogin').click()
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-button').click()
+        cy.get('#terms').click()
+        cy.get('#place_order').click()
+
+        cy.get('.woocommerce-notice').should('contain', 'Obrigado. Seu pedido foi recebido.')
     });
+
 
 
 })
